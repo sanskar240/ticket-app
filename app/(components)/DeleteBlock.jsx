ac1d7019d1ab8700e3c239159187149daac6041a
sanskar240 <71585678+sanskar240@users.noreply.github.com>
2023-12-26 19:53:51 +0530

    added a delete router

--- app/(components)/DeleteBlock.jsx
+++ app/(components)/DeleteBlock.jsx
@@ -1,15 +1,28 @@
 "use client";
 
-const { faX } = require("@fortawesome/free-solid-svg-icons");
-const { FontAwesomeIcon } = require("@fortawesome/react-fontawesome");
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
 
-const DeleteBlock = () =>{
-    return (
-        <FontAwesomeIcon
-        icon = {faX}
-        className=" text-red-400 hover:cursor-pointer hover:text-red-200"
+const DeleteBlock = ({ id }) => {
+  const router = useRouter();
 
-        />
-    );
-}
+  const deleteTicket = async () => {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      method: "DELETE",
+    });
+    if (res.ok) {
+      router.refresh();
+    }
+  };
+
+  return (
+    <FontAwesomeIcon
+      icon={faX}
+      className=" text-red-400 hover:cursor-pointer hover:text-red-200"
+      onClick={deleteTicket}
+    />
+  );
+};
+
 export default DeleteBlock;
--- app/api/Tickets/[id]/route.js
+++ app/api/Tickets/[id]/route.js
@@ -3,6 +3,9 @@ import {NextResponse} from "next/server";
 
 export async function DELETE(req,{params}){
     try{
+        const {id} = params
+        await Ticket.findByIdAndDelete(id);
+        return NexttResponse.json({meesage:"Ticket Deleted"},{status:200})
 
     }catch(error) {
         return NextResponsse.json({message:"Error",error},{status:400})
