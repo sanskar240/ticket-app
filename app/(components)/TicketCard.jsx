76158f2182b051bbdfc07190b7d7a91119501895
sanskar240 <71585678+sanskar240@users.noreply.github.com>
2023-12-26 19:28:10 +0530

    get,post operational

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -1,32 +1,54 @@
+import StatusDisplay from "./StatusDisplay";
+import DeleteBlock from "./DeleteBlock";
+import ProgressBar from "./ProgressBar";
+import Link from "next/link";
+import PriorityDisplay from "./PriorityDisplay";
 
-import DeleteBlock from './DeleteBlock'
-import PriorityDisplay from './PriorityDisplay';
-import ProgressBar from './ProgressBar';
-import StatusDisplay from './StatusDisplay';
+const TicketCard = ({ ticket }) => {
+  function formatTimestamp(timestamp) {
+    const options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+    };
 
-const TicketCard = () => {
+    const date = new Date(timestamp);
+    const formattedDate = date.toLocaleString("en-US", options);
+
+    return formattedDate;
+  }
+
+  const createdDateTime = formatTimestamp(ticket.createdAt);
+
   return (
-    <div className='flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2' >
-        <div className=' flex mb-3'>
-        <div className='ml-auto'>
-        <DeleteBlock/>
+    <div className="flex flex-col hover:bg-card-hover bg-card rounded-md shadow-lg p-3 m-2">
+      <div className="flex mb-3">
+        <PriorityDisplay priority={ticket.priority} />
+        <div className="ml-auto">
+          <DeleteBlock id={ticket._id} />
         </div>
-        <PriorityDisplay/>
-        </div>
-        <h4>Ticket Title</h4>
-        <hr className='h-px border-o bg-page mb-2'/>
-        <p className='whitespace-pre-wrap'>description</p>
-        <div className='flex-grow'></div>
-        <div className='flex mt-2'>
-            <div className='flex flex-col'>
+      </div>
+      <Link href={`/TicketPage/${ticket._id}`} style={{ display: "contents" }}>
+        <h4 className="mb-1">{ticket.title}</h4>
+        <hr className="h-px  border-0 bg-page mb-2 "></hr>
+        <p className="whitespace-pre-wrap">{ticket.description}</p>
 
-            </div>
-        <ProgressBar/>
-        
-        <StatusDisplay/>
+        <div className="flex-grow"></div>
+        <div className="flex mt-2">
+          <div className="flex flex-col">
+            <p className="text-xs  my-1">{createdDateTime}</p>
+            <ProgressBar progress={ticket.progress} />
+          </div>
+          <div className="ml-auto  flex items-end">
+            <StatusDisplay status={ticket.status} />
+          </div>
         </div>
-        </div>
-  )
-}
+      </Link>
+    </div>
+  );
+};
 
 export default TicketCard;
